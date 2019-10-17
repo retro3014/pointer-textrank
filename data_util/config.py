43ef5d748f4249377e42e383ded72009c6f908ed
gs18075 <hm2839@naver.com>
2019-10-17 11:40:22 +0900

    asd

--- data_util/config.py
+++ data_util/config.py
@@ -10,20 +10,20 @@ vocab_path = os.path.join(root_dir, "ptr_nw/cnn-dailymail-master/finished_files/
 log_root = os.path.join(root_dir, "ptr_nw/log")
 
 # Hyperparameters
-hidden_dim= 256
-emb_dim= 128
-batch_size= 8
-max_enc_steps=400
-max_dec_steps=100
-beam_size=4
-min_dec_steps=35
-vocab_size=50000
+hidden_dim = 256
+emb_dim = 128
+batch_size = 8
+max_enc_steps = 400
+max_dec_steps = 100
+beam_size = 4
+min_dec_steps = 35
+vocab_size = 50000
 
-lr=0.15
-adagrad_init_acc=0.1
-rand_unif_init_mag=0.02
-trunc_norm_init_std=1e-4
-max_grad_norm=2.0
+lr = 0.15
+adagrad_init_acc = 0.1
+rand_unif_init_mag = 0.02
+trunc_norm_init_std = 1e-4
+max_grad_norm = 2.0
 
 pointer_gen = True
 is_coverage = False
@@ -32,6 +32,6 @@ cov_loss_wt = 1.0
 eps = 1e-12
 max_iterations = 500000
 
-use_gpu=True
+use_gpu = True
 
-lr_coverage=0.15
+lr_coverage = 0.15
