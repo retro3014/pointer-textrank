61453a3e71eead0c7e048f329d916bcef43939ff
hm2839 <hm2839@naver.com>
2019-10-22 08:41:35 +0900

    10220841

--- start_train.sh
+++ start_train.sh
@@ -1,3 +1,2 @@
 export PYTHONPATH=`pwd`
-/home/dmlab/anaconda3/bin/python3 training_ptr_gen/train.py >& log/training_log &
-
+CUDA_VISIBLE_DEVICES="1" /home/dmlab/anaconda3/bin/python3 training_ptr_gen/train.py #>& log/training_log &
