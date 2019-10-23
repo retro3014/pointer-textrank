3f287f181d410a849c12576506a3850f2094715d
root <retro3014@naver.com>
2019-10-23 14:49:24 +0900

    editC

--- start_train.sh
+++ start_train.sh
@@ -1,12 +1,4 @@
 export PYTHONPATH=`pwd`
-<<<<<<< HEAD
-CUDA_VISIBLE_DEVICES="1" python training_ptr_gen/train.py >& ../log/training_log &
-=======
-<<<<<<< HEAD
-CUDA_VISIBLE_DEVICES="1" /home/dmlab/anaconda3/bin/python3 training_ptr_gen/train.py #>& log/training_log &
-=======
-CUDA_VISIBLE_DEVICES="1"
-python training_ptr_gen/train.py >& ../log/training_log &
->>>>>>> 5ba5f9ca9eadfbebf880f2a6746e1d3c6844ef04
 
->>>>>>> 01fa3dfa735cf6f96c96277830034e0437594938
+CUDA_VISIBLE_DEVICES="1" /home/dmlab/anaconda3/bin/python3 training_ptr_gen/train.py #>& log/training_log &
+
